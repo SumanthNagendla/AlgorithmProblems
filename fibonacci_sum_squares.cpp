277cbadee41f592cb01ff6e238c832b9306fd5f8
SumanthNagendla <34907759+SumanthNagendla@users.noreply.github.com>
2021-01-29 16:57:09 -0500

    Sum of squares of last digits of Fibonnaci numbers - Algorithms

---
+++ fibonacci_sum_squares.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+int fibonacci_sum_squares_naive(long long n) {
+    if (n <= 1)
+        return n;
+
+    long long previous = 0;
+    long long current  = 1;
+    long long sum      = 1;
+
+    for (long long i = 0; i < n - 1; ++i) {
+        long long tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+        sum += current * current;
+    }
+
+    return sum % 10;
+}
+
+long long fibonacci_fast(long long n) {
+  n = n%60;
+    if (n <= 1)
+        return n;
+    long long previous = 0;
+    long long current  = 1;
+    long long tmp_previous = -1;
+    long long sum = 1;
+
+    for (long long i = 0; i < n - 1; ++i) {
+        tmp_previous = previous;
+        previous = current;
+        current = (tmp_previous + current)%10;
+    }
+    return current;
+}
+
+long long fibonacci_sum_squares_fast(long long n) {
+  n = n%60;
+    if (n <= 1)
+        return n;
+    long long previous = 0;
+    long long current  = 1;
+    long long tmp_previous = -1;
+    long long sum = 1;
+
+    for (long long i = 0; i < n - 1; ++i) {
+        tmp_previous = previous;
+        previous = current;
+        current = (tmp_previous + current)%10;
+    }
+    return (current*(current+previous))%10;
+}
+
+int main() {
+    long long n = 0;
+    std::cin >> n;
+    std::cout << fibonacci_sum_squares_fast(n);
+}
